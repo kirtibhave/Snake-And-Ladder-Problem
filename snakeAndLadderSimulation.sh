547f2d0d382cbf29e5c24d31ed15ed859d74b257
kirtibhave <kirtibhave1000@gmail.com>
2020-03-09 12:16:46 +0530

    ADDED:While loop to check player reaches till winning position

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -5,35 +5,47 @@ echo "**********Welcome to Snake & Ladder Problems**********"
 NO_PLAY=0
 LADDER=1
 SNAKE=2
+STARTPOSITION=0
+WINNINGPOSITION=100
 
 #VARIABLES
-start_Postion=0
 position=0
 
 #function is used to rolls the die to get a number
 function DieRolls(){
-	randomNumber=$(($RANDOM%6+1))
-	echo $randomNumber
+RandomNumber=$(($RANDOM%6+1))
+echo $RandomNumber
 }
 
 #function is used to checks option whether they are no play,ladder or snake
 function toChecksOption(){
-	checkOption=$(($RANDOM%3))
-	dieGeneratedValues=$(DieRolls)
-		case $checkOption in
-			0)
-				position=$position
-				;;
-			1)
-				position=$(($position+$dieGeneratedValues))
-				;;
-			2)
-				position=$(($position-$dieGeneratedValues))
-				;;
-			*)
-				echo "enter proper choice"
-				;;
-		esac
+checkOption=$(($RANDOM%3))
+dieGeneratedValues=$(DieRolls)
+case $checkOption in
+	0)
+		position=$position
+		;;
+	1)
+		position=$(($position+$dieGeneratedValues))
+		;;
+	2)
+		position=$(($position-$dieGeneratedValues))
+	   ;;
+	*)
+		echo "enter proper choice"
+		;;
+esac
 }
-toChecksOption
 
+#it is used to repeat till the player reaches the winning position
+while [ $position -le $WINNINGPOSITION ]
+do
+	toChecksOption
+done
+
+#it is used check if player position moves below 0
+if [ $position -le 0 ]
+then
+	position=$STARTPOSITION
+fi
+	echo $position
