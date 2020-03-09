e3d2895fd653d0cbb657334bf668d71400e064de
kirtibhave <kirtibhave1000@gmail.com>
2020-03-09 10:39:09 +0530

    ADDED:function to generate random number on die

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -3,3 +3,10 @@ echo "**********Welcome to Snake & Ladder Problems**********"
 
 #VARIABLES
 start_Position=0
+
+#function is used to rolls the die to get a number
+function dieRolls(){
+	dieRoll=$((RANDOM%6+1))
+	echo "dieroll is $dieRoll"
+}
+dieRolls
