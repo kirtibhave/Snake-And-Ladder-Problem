4e210850b0fbecb7697009bc3219fc3c60d7516a
kirtibhave <kirtibhave1000@gmail.com>
2020-03-09 11:23:20 +0530

    ADDED:Function to checks for options No play,Ladder or Snake

--- snakeAndLadderSimulation.sh
+++ snakeAndLadderSimulation.sh
@@ -1,12 +1,39 @@
 #!/bin/bash -x
 echo "**********Welcome to Snake & Ladder Problems**********"
 
+#CONSTANTS
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
 #VARIABLES
-start_Position=0
+start_Postion=0
+position=0
 
 #function is used to rolls the die to get a number
-function dieRolls(){
-	dieRoll=$((RANDOM%6+1))
-	echo "dieroll is $dieRoll"
+function DieRolls(){
+	randomNumber=$(($RANDOM%6+1))
+	echo $randomNumber
 }
-dieRolls
+
+#function is used to checks option whether they are no play,ladder or snake
+function toChecksOption(){
+	checkOption=$(($RANDOM%3))
+	dieGeneratedValues=$(DieRolls)
+		case $checkOption in
+			0)
+				position=$position
+				;;
+			1)
+				position=$(($position+$dieGeneratedValues))
+				;;
+			2)
+				position=$(($position-$dieGeneratedValues))
+				;;
+			*)
+				echo "enter proper choice"
+				;;
+		esac
+}
+toChecksOption
+
